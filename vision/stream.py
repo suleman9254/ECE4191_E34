ac27ba8ce6b0faa79b49c58cfde9dc99c777a6bf
suleman9254 <suleman9254@yahoo.com>
2024-08-24 17:32:26 +1000

    supports dynamic frame changes

--- vision/stream.py
+++ vision/stream.py
@@ -3,33 +3,21 @@ import numpy as np
 from flask import Flask, Response
 
 class Stream(object):
-    def __init__(self, camera, detector, host='0.0.0.0', port=5000):        
-        
-        self.host = host
-        self.port = port
-        
-        self.camera = camera
-        self.detector = detector
-
+    def __init__(self, host='0.0.0.0', port=5000):        
         self.app = Flask(__name__)
         self.app.add_url_rule('/', 'video_feed', self.video_feed)
-    
-    def get_frame(self):
-        while True:
-            frame = self.camera.read_frame()
-            circle = self.detector.find_ball(frame)
-            
-            if circle is not None:
-                circle = np.round(circle).astype("int")
-                
-                for (x, y, r) in circle:
-                    cv.circle(frame, (x, y), r, (0, 255, 0), 4)
-                    cv.circle(frame, (x, y), 2, (0, 0, 255), 3)
-            
-            _, buffer = cv.imencode('.jpg', frame)
-            frame = buffer.tobytes()
 
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        self.host, self.port = host, port
+        self.frame = np.zeros((720, 1280, 3))
+    
+    def set_frame(self, frame):
+        self.frame = frame
+
+    def get_frame(self):
+        _, buffer = cv.imencode('.jpg', self.frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     
     def video_feed(self):
         return Response(self.get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
