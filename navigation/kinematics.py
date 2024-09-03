4ff61a1be3e93394c123d37eb8056bfe0ad1b7ec
suleman9254 <suleman9254@yahoo.com>
2024-09-03 21:34:21 +1000

    subdivided pose update to smaller methods

--- navigation/kinematics.py
+++ navigation/kinematics.py
@@ -1,5 +1,5 @@
-import time
 import math
+from threading import Thread
 
 class DiffDriveModel(object):
     def __init__(self, motor_l, motor_r, wheel_radius=0.05, wheel_sep=0.15, dt=0.08, encoder_delay=0.04):
@@ -22,27 +22,29 @@ class DiffDriveModel(object):
         self.left_motor = motor_l
 
         self.encoder_delay = encoder_delay
-    
-    # Veclocity motion model
-    def update_velocity(self):
-        
+
+    def update_wheel_velocities(self):
         self.wl = self.left_motor.read_velocity(self.encoder_delay)
         self.wr = self.right_motor.read_velocity(self.encoder_delay)
-        
+
+    def update_base_velocities(self):
         self.v = (self.wl*self.r - self.wr*self.r) / 2.0
         self.w = (self.wl*self.r + self.wr*self.r) / self.l
     
-    def change_pwm(self, duty_cycle_l, duty_cycle_r):
-        
-        self.left_motor.change_pwm(duty_cycle_l)
-        self.right_motor.change_pwm(duty_cycle_r)
-
-    # Kinematic motion model
-    def pose_update(self, duty_cycle_l, duty_cycle_r):
-        
-        self.change_pwm(duty_cycle_l, duty_cycle_r)
-        self.update_velocity()
-                
+    def update_position(self):
         self.x = self.x + self.dt*self.v*math.cos(self.th)
         self.y = self.y + self.dt*self.v*math.sin(self.th)
         self.th = self.th + self.w*self.dt
+
+    def change_pwm(self, duty_cycle_l, duty_cycle_r):
+        self.left_motor.change_pwm(duty_cycle_l)
+        self.right_motor.change_pwm(duty_cycle_r)
+
+    def pose_update(self, duty_cycle_l, duty_cycle_r):
+        
+        self.change_pwm(duty_cycle_l, 
+                        duty_cycle_r)
+        
+        self.update_wheel_velocities()
+        self.update_base_velocities()
+        self.update_position()
