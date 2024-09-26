b451d9c2136c08c3a04b189e1d39265c09d8cf88
Suleman <suleman9254@yahoo.com>
2024-09-26 22:10:18 +1000

    disable servo when not in use

--- transducers/grabber.py
+++ transducers/grabber.py
@@ -2,18 +2,21 @@ from time import sleep
 from gpiozero import Servo
 
 class Claw(object):
-    def __init__(self, pin, delay=0.3, initial_value=1, min_pulse_width=0.0004, max_pulse_width=0.0024):
+    def __init__(self, pin, delay=0.3, min_pulse_width=0.0004, max_pulse_width=0.0024, frame_width=0.02):
         
         self.delay = delay
         self.servo = Servo(pin=pin, 
-                           initial_value=initial_value, 
+                           initial_value=None, 
                            min_pulse_width=min_pulse_width, 
-                           max_pulse_width=max_pulse_width)
+                           max_pulse_width=max_pulse_width, 
+                           frame_width=frame_width)
     
     def grab(self):
         self.servo.min()
         sleep(self.delay)
+        self.servo.detach()
     
     def release(self):
         self.servo.max()
         sleep(self.delay)
+        self.servo.detach()
