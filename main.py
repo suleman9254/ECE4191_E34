d86cbfbc8ba036ae99978177febf8ed1c3aa49f0
Suleman <suleman9254@yahoo.com>
2024-10-08 22:15:46 +1100

    tried to finish task

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from navigation.kinematics import DiffDriveModel
 from navigation.controller import PIController
 from navigation.planner import TentaclePlanner
 
-from transducers.grabber import Claw
-from transducers.sensor import DistanceSensor
 from transducers.stepper import Rail
+from transducers.grabber import Claw
+from transducers.sensors import Ultrasonic, ToFPanel
 
 from vision.camera import Camera
 from vision.detector import YOLODetector
 
-from system.robot import Robot
+from system.alt import Robot
 
 max_count = 48 * 75
 motorL = Motor(in1=5, in2=6, enable=13, freq=100, encA=23, encB=24, max_count=max_count)
 motorR = Motor(in1=17, in2=27, enable=22, freq=100, encA=19, encB=26, max_count=max_count)
 
-claw, ultrasonic = Claw(pin=18), DistanceSensor()
-rail = Rail(motor_pins=[12, 16, 20, 21], start_pos=0, total_len=160)
+claw = Claw(pin=18)
+rail = Rail(motor_pins=[12, 16, 20, 21])
+
+tof = ToFPanel(pins=[7, 8, 25])
+ultrasonic = Ultrasonic(trig=[9, 10], echo=[11, 14])
 
 encoder_delay = 0.02
 dt = 2 * encoder_delay + 0.010424973
@@ -30,23 +33,24 @@ plannerSteps = 5*3
 v, w = np.arange(-0.2, 0.3, 0.025), np.arange(-0.3, 0.3, 0.1)
 planner = TentaclePlanner(v, w, steps=plannerSteps, alpha=1, beta=0.1, dt=dt)
 
-box_ckpt, ball_ckpt = r'vision/ckpts/box_yolo11_new.pt', r'vision/ckpts/balls.pt'
-camera, detector = Camera(0), YOLODetector(box_ckpt=box_ckpt, ball_ckpt=ball_ckpt, thresh=0.1)
+box_ckpt, ball_ckpt = r'vision/ckpts/yolov11_box_V2.pt', r'vision/ckpts/balls.pt'
+camera, detector = Camera(0), YOLODetector(box_ckpt=box_ckpt, ball_ckpt=ball_ckpt, thresh=0.6)
 
 params = {'xBox': 5.48, 'yBox': -4.11, 'xBound': 3, 'yBound': 2.2, 'ballRadius': 0.0325, 'boxHeight': 0.16}
-robot = Robot(model, controller, planner, claw, rail, ultrasonic, camera, detector, params)
+robot = Robot(model, controller, planner, claw, rail, tof, ultrasonic, camera, detector, params)
+
+onTime = 5 * 60
+
+tgtBoxProx = (0.6, 0.4)
+tgtBallProx = (0.5, 0.3)
+
+deliverDist = 0.02
+grabDist = [0.09, 0.06, 0.05]
+
+ballAngleTolerance = 0
+boxAngleTolerance = 10 * 2 * np.pi / 360
 
 beta = 0.8
 alpha = 0.6
 
-grabDist = 0.04
-maxCamBoxDist = 0.6
-maxCamBallDist = 0.3
-alphaReductionDist = 0.6
-
-boxAngleTolerance = 0.2
-ballAngleTolerance = 0
-
-onTime = 5 * 60
-
-robot.start(onTime, maxCamBallDist, maxCamBoxDist, grabDist, boxAngleTolerance, ballAngleTolerance, alphaReductionDist, alpha, beta)
+robot.start(onTime, tgtBallProx, tgtBoxProx, ballAngleTolerance, boxAngleTolerance, grabDist, deliverDist, alpha, beta)
